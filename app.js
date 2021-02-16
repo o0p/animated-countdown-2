bb3eb5883b5e1cb9d519be882328933540b5c33c
Russell <russelldev@protonmail.com>
2021-02-16 18:48:56 +0000

    Add runAnimation funct

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const nums = document.querySelectorAll('.nums span');
 const counter = document.querySelector('.counter');
 const finalMessage = document.querySelector('.final');
 const replay = document.querySelector('#replay');
+
+runAnimation();
+
+function runAnimation() {
+    nums.forEach((num, idx) => {
+        const nextToLast = nums.length - 1;
+        num.addEventListener('animataionend', (e) => {
+            if (e.animationName === 'goIn' && idx !== nextToLast) {
+                num.classList.remove('in');
+                num.classList.add('out');
+            };
+        });
+    });
+};
